570d02ce395f58041614dfc68bb8aff80a85ffe1
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2024-07-09 00:02:57 -0300

    feat: project settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/Auth/SignIn';
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
