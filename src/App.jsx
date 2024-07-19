e814ec9985be16b3b01a92a5084f044d5668b57d
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2024-07-19 10:08:31 -0300

    config deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import AccessLogin from './pages/Auth/AccessLogin';
 import SignIn from './pages/Auth/SignIn';
