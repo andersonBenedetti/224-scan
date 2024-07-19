1232ea6525d5d06ff5ab4b21a6920ed1effe1a60
Leandro Thiago Ribeiro <111009157+LeandroTRibeiro@users.noreply.github.com>
2024-07-19 20:39:40 -0300

    feat: add new colors to lead with progress components

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -11,6 +11,10 @@ export const theme = {
     backgroundSettings: '#00000082',
     status: '#00FF38',
     whiteOpacity: '#D0CFD1',
+    primary: "#FFFFFF",
+    off_progress: "#302C40",
+    error: "#FF0000"
+
   },
 };
 
